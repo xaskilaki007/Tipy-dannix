d55a6d75b3478d47fc8d0fb8729a365550b0026d
xaskilaki007 <115955213+xaskilaki007@users.noreply.github.com>
2022-11-30 23:25:24 +0300

    Update 7var_2zad.cpp

--- 7var_2zad.cpp
+++ 7var_2zad.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <string>
+#include <cmath>
 
+
+void calculation()
+{
+	float x = 0, c = 0, m=0, z=0;
+	std::cin >> x;
+	std::cin >> c;
+	z = (tan((M_PI*m)/(m+pow(10,-3))))*(tan((M_PI*m)/(m+pow(10,-3))));
+	m = exp(-c*abs(x)+sqrt(c*x));
+	std::cout << "z" << z <<"\n";
+	std::cout << "m" << m;
+}
+int main()
+{
+
+	calculation();
+}
